60364a21ded28870fa2a5e9cab6c6b2686a4c2f6
AndriiP <andrey.pslk@gmail.com>
2022-09-15 22:31:58 +0300

    initial commit

---
+++ src/test/java/com/test/taskchat/TaskChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.taskchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
